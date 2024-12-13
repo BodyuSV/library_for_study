a4c3878918e1ea06bb4cce3a85a414505e6a6238
Stas <bodustanislav2@gmail.com>
2024-12-13 21:50:37 +0300

    Предварительная модель для статей

--- page_book/models.py
+++ page_book/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=10000)
+    photo = models.ImageField(upload_to='image/%Y/%m/%d/')
+    file = models.FileField(blank=True, upload_to='file/%Y/%m/%d/')
+    time_create = models.DateTimeField(auto_created=True)
+    is_published = models.BooleanField(default=False)
